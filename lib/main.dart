e2be67b221affeaed06823caa4f295bc2f27bdbc
Shahriar Toky <shahriartoky555@gail.com>
2023-09-26 19:07:23 +0600

    until cart button

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,12 @@ class HomeScreen extends StatefulWidget {
 
 
   List <dynamic> productDetails =[
-    { 'name' : 'Product 1' , 'price' : "100"},
-    { 'name' : 'Product 2' , 'price' : "200"},
-    { 'name' : 'Product 3' , 'price' : "1400"},
-    { 'name' : 'Product 4' , 'price' : "1040"},
-    { 'name' : 'Product 5' , 'price' : "2020"},
-    { 'name' : 'Product 6' , 'price' : "500"},
+    { 'name' : 'Product 1' , 'price' : "100", 'counter': 0},
+    { 'name' : 'Product 2' , 'price' : "200",'counter': 0},
+    { 'name' : 'Product 3' , 'price' : "1400",'counter': 0},
+    { 'name' : 'Product 4' , 'price' : "1040",'counter': 0},
+    { 'name' : 'Product 5' , 'price' : "2020",'counter': 0},
+    { 'name' : 'Product 6' , 'price' : "500",'counter': 0},
 
   ] ;
 
@@ -40,12 +40,14 @@ class _HomeScreenState extends State<HomeScreen> {
     MyAlertDialog(context,productName)
     {
       return showDialog(context: context, builder: (BuildContext context){return Expanded(child: AlertDialog(title: const Text('Congratulations!'),
-      content: Text("You've bought $counter $productName "),actions: [ElevatedButton(onPressed: (){Navigator.pop(context);}, child: Text('OK'))],));});
+      content: Text("You've bought 5 $productName "),actions: [ElevatedButton(onPressed: (){Navigator.pop(context);}, child: const Text('OK'))],));});
     }
     return Scaffold(
       appBar: AppBar(title: const Text('Product List'),
       centerTitle: true,),
-      body: ListView.separated(itemBuilder: (context , index){
+      body:
+      ListView.separated(itemBuilder: (context , index){
+      // int productCounter = widget.productDetails[index]['counter'];
         return ListTile(
           title: Text(widget.productDetails[index]['name']),
         subtitle: Text(widget.productDetails[index]['price']),
@@ -55,13 +57,17 @@ class _HomeScreenState extends State<HomeScreen> {
               Text('Counter : '),
               const SizedBox(height: 3),
               ElevatedButton(onPressed: (){
-               counter++;
-               if (counter==5)
+
+               setState(() {counter++;
+               widget.productDetails[index]['counter']++;
+
+               if (widget.productDetails[index]['counter']==5)
                {
                  MyAlertDialog(context,widget.productDetails[index]['name']);
-               }
-               setState(() {});
+                 widget.productDetails[index]['counter'] = 0 ;
+               }});
                 print(counter);
+                print(widget.productDetails[index]['counter']);
               },child: const Text('Buy Now'),),
             ],
           ),
